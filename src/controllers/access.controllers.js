b2c55e52d34d6f7233ed658fa8f704c606675b88
GomezFrannco <frangomez.baires@gmail.com>
2022-07-13 18:32:33 -0300

    [Updt] Access controllers has been refactored

--- src/controllers/access.controllers.js
+++ src/controllers/access.controllers.js
@@ -1,79 +1,68 @@
-const { cartDao } = require("../../API/models/cart.models.js");
+const { Cart } = require("../../API/DAO/cartsImpl.dao.js");
 const { sendMail } = require("../utils/mail.utils.js");
+const Connection = require('../DAO/connection.dao.js');
 
-// =================LOGIN====================
-async function getLogin(_req, res) {
-  return res.status(200).render("pages/access.pages.ejs", {
-    title: "Login",
-    failed: "",
-    partial: "login.partials.ejs",
-  });
-}
-async function postLogin(req, res) {
-  return res.status(200).redirect("/main");
-}
-
-async function failLogin(_req, res) {
-  return res.status(403).render("pages/access.pages.ejs", {
-    title: "Login",
-    failed: "username or password is not valid!",
-    partial: "login.partials.ejs",
-  });
-}
-
-// ================SIGNUP====================
-async function getSignup(_req, res) {
-  return res.status(200).render("pages/access.pages.ejs", {
-    title: "Signup",
-    failed: " ",
-    partial: "signup.partials.ejs",
-  });
-}
-async function postSignup(req, res) {
-  cartDao.create({ owner: req.user.name });
-  const user = 
-  `<h1>${req.user.name} is now registered</h1>
-   <h2>User data:</h2>
-    <ul>
-      <li><p>email: ${req.user.email}</p></li>
-      <li><p>name: ${req.user.name}</p></li>
-      <li><p>age: ${req.user.age}</p></li>
-      <li><p>adress: ${req.user.adress}</p></li>
-      <li><p>phone: ${req.user.phone}</p></li>
-    </ul>`
-  sendMail(user);
-  return res.status(200).redirect("/login");
-}
-
-async function failSignup(_req, res) {
-  return res.status(500).render("pages/access.pages.ejs", {
-    title: "Signup",
-    failed: "something happened, please try again",
-    partial: "signup.partials.ejs",
-  });
+class AccessControllers {
+  static async getLogin(_req, res) {
+    return res.status(200).render("pages/access.pages.ejs", {
+      title: "Login",
+      failed: "",
+      partial: "login.partials.ejs",
+    });
+  }
+  static async postLogin(_req, res) {
+    return res.status(200).redirect("/main");
+  }
+  static async failLogin(_req, res) {
+    return res.status(403).render("pages/access.pages.ejs", {
+      title: "Login",
+      failed: "username or password is not valid!",
+      partial: "login.partials.ejs",
+    });
+  }
+  static async getSignup(_req, res) {
+    return res.status(200).render("pages/access.pages.ejs", {
+      title: "Signup",
+      failed: " ",
+      partial: "signup.partials.ejs",
+    });
+  }
+  static async postSignup(req, res) {
+    await Cart.addCart({ owner: req.user.name });
+    sendMail(req.user);
+    return res.status(200).redirect("/login");
+  }
+  static async failSignup(_req, res) {
+    return res.status(500).render("pages/access.pages.ejs", {
+      title: "Signup",
+      failed: "something happened, please try again",
+      partial: "signup.partials.ejs",
+    });
+  }
+  static async getLogout(req, res) {
+    const connection = Connection.getInstance();
+    if (connection.status == true) {
+      await connection.disconnect()
+    }
+    await req.logout({}, (err) => {
+      res.send(err);
+    });
+    res.status(200).redirect("/login");
+  }
 }
 
-// ================LOGOUT====================
-async function getLogout(req, res) {
-  req.logout({}, (err) => {
-    res.send(err);
-  });
-  res.status(200).redirect("/login");
-}
-
-// ================EXPORT====================
 module.exports = {
   get: {
-    login: getLogin,
-    signup: getSignup,
-    logout: getLogout,
+    login: AccessControllers.getLogin,
+    signup: AccessControllers.getSignup,
+    logout: AccessControllers.getLogout,
   },
   post: {
-    login: postLogin,
-    signup: postSignup,
+    login: AccessControllers.postLogin,
+    signup: AccessControllers.postSignup,
   },
   fail: {
-    login: failLogin,
-    signup: failSignup,
+    login: AccessControllers.failLogin,
+    signup: AccessControllers.failSignup,
   },
 };
