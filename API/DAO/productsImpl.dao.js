c5217f9c9bf66a88090be82885b6731213664572
GomezFrannco <frangomez.baires@gmail.com>
2022-07-13 18:17:46 -0300

    [Updt] cart/product DAO classes updated

--- API/DAO/productsImpl.dao.js
+++ API/DAO/productsImpl.dao.js
@@ -1,61 +1,49 @@
 const ProductInterface = require("../interfaces/products.interfaces.js");
 const { productModel } = require("../schemas/products.schemas.js");
-const Connection = require("./connection.dao.js");
 
 class ProductDAO extends ProductInterface {
   constructor() {
     super();
-    this.connection = Connection.getInstance();
     this.db = productModel;
   }
   async addProduct(product) {
     try {
-      await this.connection.connect();
       return await this.db.create(product);
     } catch (error) {
       console.log(error.message);
     } finally {
-      await this.connection.disconnect();
     }
   }
   async getProduct(id) {
     try {
-      await this.connection.connect();
       return await this.db.findOne({ id: id });
     } catch (error) {
       console.log(error.message);
     } finally {
-      await this.connection.disconnect();
     }
   }
   async getAll() {
     try {
-      await this.connection.connect();
       return await this.db.find();
     } catch (error) {
       console.log(error.message);
     } finally {
-      await this.connection.disconnect();
     }
   }
   async updateProduct(id, update) {
     try {
-      await this.connection.connect();
       return await this.db.findOneAndUpdate({ id: id }, update);
     } catch (error) {
       console.log(error.message);
     } finally {
-      await this.connection.disconnect();
     }
   }
   async removeProduct(id) {
     try {
-      await this.connection.connect();
       return await this.db.findOneAndDelete({ id: id });
     } catch (error) {
       console.log(error.message);
     } finally {
-      await this.connection.disconnect();
     }
   }
 }
