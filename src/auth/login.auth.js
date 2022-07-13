fc435b0954b7670ee191c83f994bcf6f21b6900c
GomezFrannco <frangomez.baires@gmail.com>
2022-07-13 18:34:16 -0300

    [Updt] Passport strategies has been updated

--- src/auth/login.auth.js
+++ src/auth/login.auth.js
@@ -1,22 +1,25 @@
 const { Strategy } = require("passport-local");
 const passport = require("passport");
 const bcrypt = require("bcrypt");
-const { userModel } = require("../models/user.models.js");
+const { User } = require('../DAO/userImpl.dao.js');
 const init = require("../utils/passport.utils.js");
 const { log } = require('../utils/logs.utils.js');
+const Connection = require('../DAO/connection.dao.js');
 
-passport.use(
-  "login",
-  new Strategy(async (name, password, done) => {
+passport.use("login", new Strategy(async (name, password, done) => {
     try {
-      const findUser = await userModel.findOne({ name });
-      if (!findUser) {
+      const connection = Connection.getInstance();
+      if (connection.status == false) {
+        await connection.connect()
+      }
+      const find = await User.getUser({ name });
+      if (!find) {
         done(null, false);
       } else {
-        const comparison = bcrypt.compareSync(password, findUser.password);
-        comparison ? done(null, findUser) : done(null, false);
+        const comparison = bcrypt.compareSync(password, find.password);
+        comparison ? done(null, find) : done(null, false); 
       }
-    } catch (err) {
+    } catch (err) { 
       log.console.error(err.message);
       log.file.error(err.message);
     }
--- src/auth/signup.auth.js
+++ src/auth/signup.auth.js
@@ -1,41 +1,43 @@
 const { Strategy } = require("passport-local");
 const passport = require("passport");
 const bcrypt = require("bcrypt");
-const { userModel } = require("../models/user.models.js");
+const { User } = require("../DAO/userImpl.dao.js");
 const init = require("../utils/passport.utils.js");
-const { log } = require('../utils/logs.utils.js');
+const { log } = require("../utils/logs.utils.js");
+const Connection = require('../DAO/connection.dao.js');
 
-passport.use(
-  "signup",
-  new Strategy(
-    {
-      passReqToCallback: true,
-    },
-    async (req, name, password, done) => {
-      try {
-        const exists = await userModel.findOne({ name });
-        if (exists) {
-          return done(null, false);
-        } else {
-          const newUser = {
-            email: req.body.email,
-            password: bcrypt.hashSync(password, bcrypt.genSaltSync(10), null),
-            name,
-            age: req.body.age,
-            adress: req.body.adress,
-            phone: req.body.phone,
-            picture: `${req.file ? 'uploads/' + req.file.filename : 'uploads/default-image.png'}`
-          };
-          const saveNewUser = await userModel.create(newUser);
-          return done(null, saveNewUser);
-        }
-      } catch (err) {
-        log.console.error(err.message);
-        log.file.error(err.message);
+passport.use("signup", new Strategy({ passReqToCallback: true }, async (req, name, password, done) => {
+    try {
+      const connection = Connection.getInstance();
+      if (connection.status == false) {
+        await connection.connect()
       }
-      return done(null, false);
+      const availability = await User.getUser({ name });
+      if (availability) {
+        return done(null, false);
+      } else {
+        const newUser = {
+          email: req.body.email,
+          password: bcrypt.hashSync(password, bcrypt.genSaltSync(10), null),
+          name,
+          age: req.body.age,
+          adress: req.body.adress,
+          phone: req.body.phone,
+          picture: `${
+            req.file
+              ? "uploads/" + req.file.filename
+              : "uploads/default-image.png"
+          }`,
+        };
+        const saveUser = await User.addUser(newUser);
+        return done(null, saveUser);
+      }
+    } catch (err) {
+      log.console.error(err.message);
+      log.file.error(err.message);
     }
-  )
+    return done(null, false);
+  })
 );
 
 init();
