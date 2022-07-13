1782d8ae6815e22db90c7231e5284c5ac554832d
GomezFrannco <frangomez.baires@gmail.com>
2022-07-13 18:31:39 -0300

    [Updt] Main controllers has been refactored

--- src/controllers/main.controllers.js
+++ src/controllers/main.controllers.js
@@ -1,43 +1,42 @@
 const fetch = require("node-fetch");
-const { cartDao } = require('../../API/models/cart.models.js')
+const { Cart } = require('../../API/DAO/cartsImpl.dao.js');
 
-async function getMain(req, res) {
-  let url = `${req.protocol}://${req.hostname}/api/products`
-  const request = await fetch(url, {
-    method: "GET",
-  });
-  const response = await request.json();
-  res.status(200).render("pages/home.pages.ejs", {
-    partial: "main.partials.ejs",
-    products: response,
-  });
-}
-
-function getAccount(req, res) {
-  res.status(200).render("pages/home.pages.ejs", {
-    partial: "account.partials.ejs",
-    avatar: req.user.picture,
-    username: req.user.name,
-    age: req.user.age,
-    adress: req.user.adress,
-    phone: req.user.phone,
-  });
-}
-
-async function getCart(req, res) {
-  const cart = await cartDao.readOne({owner: req.user.name });
-  const itemsQ = await cart.products.length;
-  res.status(200).render("pages/home.pages.ejs", {
-    partial: "cart.partials.ejs",
-    cart: itemsQ,
-    itemsIn: cart.products
-  });
+class MainController {
+  static async getMain(req, res) {
+    const url = `${req.protocol}://${req.hostname}:8080/api/products`
+    const request = await fetch(url, {
+      method: "GET"
+    });
+    const response = await request.json();
+    await res.status(200).render("pages/home.pages.ejs", {
+      partial: "main.partials.ejs",
+      products: response.products,
+    });
+  }
+  static getAccount(req, res) {
+    res.status(200).render("pages/home.pages.ejs", {
+      partial: "account.partials.ejs",
+      avatar: req.user.picture,
+      username: req.user.name,
+      age: req.user.age,
+      adress: req.user.adress,
+      phone: req.user.phone,
+    });
+  }
+  static async getCart(req, res) {
+    const userCart = await Cart.getCart(req.user.name);
+    res.status(200).render("pages/home.pages.ejs", {
+      partial: "cart.partials.ejs",
+      cart: userCart.products.length,
+      itemsIn: userCart.products
+    });
+  }
 }
 
 module.exports = {
   get: {
-    main: getMain,
-    account: getAccount,
-    cart: getCart,
+    main: MainController.getMain,
+    account: MainController.getAccount,
+    cart: MainController.getCart,
   },
 };
