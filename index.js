dd8049f211660a15b5b66e576a3d81553debb5e2
GomezFrannco <frangomez.baires@gmail.com>
2022-07-13 18:37:58 -0300

    [Updt] DB connection was changed & some files has been removed

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const { App } = require("./src/app.js");
 require("./src/config/dotenv.config.js");
-require('./src/config/mongoose.config.js')
 
 function main(port) {
   const app = new App(port);
